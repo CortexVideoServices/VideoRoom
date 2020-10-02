b60e27df4b48c907733e2fc2d5896abc1556e334
Artem <artm-porjad@mail.ru>
2020-10-02 20:30:37 +0300

    Added model ConfirmedToken

---
+++ samples/VideoRoom/videoroom/server.py
@@ -0,0 +1,36 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/status')
+async def get_status(request: web.Request):
+    return web.json_response({
+        'status': 'OK'
+    })
+
+@routes.get('/Artem')
+async def get_status(request: web.Request):
+    return web.json_response({
+        'status': 'alive'
+    })
+
+
+
+class Application(web.Application):
+    """ Backend web application
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_routes(routes)
+
+
+async def app_factory():
+    """ Async application factory
+    """
+    return Application()
+
+
+if __name__ == '__main__':
+    web.run_app(app_factory(), port=8001)
